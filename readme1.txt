209f35d295fc581d4b3c9ddf6c484b5ae6d9bb49
mayurmulay <mayurkmulay@gmail.com>
2026-07-20 04:47:53 +0000

    profiler

---
+++ readme1.txt
@@ -0,0 +1,16 @@
+{
+    "repository": {
+        "repositoryArn": "arn:aws:ecr:ap-northeast-1:748624204890:repository/spring-petclinic",
+        "registryId": "748624204890",
+        "repositoryName": "spring-petclinic",
+        "repositoryUri": "748624204890.dkr.ecr.ap-northeast-1.amazonaws.com/spring-petclinic",
+        "createdAt": "2026-07-15T14:21:19.277000+00:00",
+        "imageTagMutability": "MUTABLE",
+        "imageScanningConfiguration": {
+            "scanOnPush": false
+        },
+        "encryptionConfiguration": {
+            "encryptionType": "AES256"
+        }
+    }
+}
---
+++ src/main/java/org/springframework/samples/petclinic/profiler/BeanStartupProfiler.java
@@ -0,0 +1,69 @@
+package org.springframework.samples.petclinic.profiler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.SmartInitializingSingleton;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class BeanStartupProfiler
+        implements InstantiationAwareBeanPostProcessor,
+                   SmartInitializingSingleton {
+
+    private static final Logger log =
+            LoggerFactory.getLogger(BeanStartupProfiler.class);
+
+    private final Map<String, Long> startTimes = new ConcurrentHashMap<>();
+    private final Map<String, Long> durations = new ConcurrentHashMap<>();
+
+    @Override
+    public Object postProcessBeforeInstantiation(Class<?> beanClass,
+                                                 String beanName)
+            throws BeansException {
+
+        startTimes.put(beanName, System.nanoTime());
+        return null;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean,
+                                                 String beanName)
+            throws BeansException {
+
+        Long start = startTimes.remove(beanName);
+
+        if (start != null) {
+            long elapsed =
+                    (System.nanoTime() - start) / 1_000_000;
+
+            durations.put(beanName, elapsed);
+        }
+
+        return bean;
+    }
+
+    @Override
+    public void afterSingletonsInstantiated() {
+
+        log.info("");
+        log.info("============== Bean Startup Report ==============");
+
+        durations.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(
+                        Comparator.reverseOrder()))
+                .limit(20)
+                .forEach(e ->
+                        log.info("{:<45} {} ms",
+                                e.getKey(),
+                                e.getValue()));
+
+        log.info("=================================================");
+    }
+}
